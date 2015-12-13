e71ae45d4b31bec4bd6e13deee0fbd63d57955ba
Philipp Oppermann <dev@phil-opp.com>
2015-12-13 19:10:26 +0100

    The `no_std` attribute is stable now
    (cherry picked from commit 22014eb38d8fab130b679038ea8d0cea3578ca39)

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![feature(no_std, lang_items)]
+#![feature(lang_items)]
 #![no_std]
 
 extern crate rlibc;
